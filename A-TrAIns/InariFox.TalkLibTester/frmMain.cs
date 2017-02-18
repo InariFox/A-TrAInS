d55b3fd5e0be473d7407924481e2379d8bea1ed5
InariFox <foxit.uxm@gmail.com>
2017-02-19 01:22:12 +0900

    TalkLibTester:エラーメッセージ表示追加

--- A-TrAIns/InariFox.TalkLibTester/frmMain.cs
+++ A-TrAIns/InariFox.TalkLibTester/frmMain.cs
@@ -22,8 +22,17 @@ namespace InariFox.TalkLibTester
         private void btnTest_Click(object sender, EventArgs e)
         {
             string target = lboxEnabled.SelectedItem.ToString();
-            td.setText(target, "おはよう新しい私。");
-            td.Play(target);
+            bool result = false;
+
+            if(td.setText(target, "おはよう新しい私。"))
+            {
+                result = td.Play(target);
+            }
+
+            if (!result)
+            {
+                MessageBox.Show("処理時にエラーが発生しました。", "音声合成タスク処理エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
